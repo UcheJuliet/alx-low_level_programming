d678d45efcaecb33227f8d22d0f5e5c7a125278f
Uchechukwu Juliet <ucheojukwu4@gmail.com>
2022-09-12 05:12:33 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,8 @@
 
 /**
  * main - assigns a random number to int n everytime
- * it executes, and prints it 
- * Return: Always 0 (Sucess)
+ * it executes, and prints it
+ * Retuen: Always 0 (Success)
  */
 int main(void)
 {
@@ -14,16 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
 		printf("%d is positive\n", n);
-	}
 	else if (n == 0)
-	{
 		printf("%d is zero\n", n);
-	}
 	else if (n < 0)
-	{
 		printf("%d is negative\n", n);
-	}
 	return (0);
 }
