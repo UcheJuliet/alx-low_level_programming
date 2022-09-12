43cee8b6719d10c2453d58fcde7e2ec5c81db7e0
Uchechukwu Juliet <ucheojukwu4@gmail.com>
2022-09-12 14:17:21 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints the last digit of the random
+ * number stored in the variable n
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -21,4 +22,3 @@ int main(void)
 		printf("and is less than 6 and not 0\n");
 	return (0);
 }
-
