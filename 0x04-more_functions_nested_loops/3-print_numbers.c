2904b2baa8e40ca12769611537a57eeb6c70ed2b
Uchechukwu Juliet <ucheojukwu4@gmail.com>
2022-09-16 17:58:47 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints number from 0 - 9
+ * followed by a new line
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char i = '0';
+
+	while (i <= '9')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
