5d2559631a7f024d2cee803f23b36585435059a1
Uchechukwu Juliet <ucheojukwu4@gmail.com>
2022-09-22 09:38:10 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,7 +5,7 @@
  * @dest: string to append to
  * @src: string to add
  *
- * Return: a pointer 0 the resulting string
+ * Return: a pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
 {
